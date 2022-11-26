41ce9f575524ad84a2e6922fb42c740f4ef154d0
Herberth Leão <herberth.leao@pm.me>
2022-11-26 14:45:50 -0300

    Add static directory

--- soph/settings.py
+++ soph/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR, 'assets')
+STATIC_URL = 'assets/'
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'soph', 'assets')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
